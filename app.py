9e8a794cadcacc3632c5cce7d709fbbea3c300a3
zain22ai <zeinaasasfeh@gmail.com>
2026-09-14 21:40:17 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+a=10
+print(a)
+print(type(a))
+a='eee'
+print(a)
+print(type(a))
+for x in a :
+    print(x*2)
+#----------------------
+z='abcdefjhiklmn'
+for x in z[0:4]:
+    print(x)
+c='Hello there!'
+print(c[-4:])
+#--------------------------------
+s=['apple', 'orange', 'banana']
+for x in s[0:2] :
+    for y in x[0:3]:
+        print(y)
+print(type(s))
+fruits = ["apple", "banana", "cherry"]
+print( "banana" in fruits)
+p="zainnn"
+p=p.upper()
+print(p)
+e= input("enter your name:")
+w=int(input('enter your age: '))
+print(f"my name is {e} , and my age is {w}") 
+print(type(w))
